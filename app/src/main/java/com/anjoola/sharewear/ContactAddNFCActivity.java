6e14d072e57c97cbb09be59c25086e7fd5db6ec7
Angela Gong <anjoola@anjoola.com>
2015-06-26 15:41:34 -0700

    Added animations for NFC adding

--- app/src/main/java/com/anjoola/sharewear/ContactAddNFCActivity.java
+++ app/src/main/java/com/anjoola/sharewear/ContactAddNFCActivity.java
@@ -1,23 +1,45 @@
 package com.anjoola.sharewear;
 
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
 
 /**
- * TODO
+ * Activity to exchange contact information over NFC. Can switch to manual
+ * input if NFC is not turned on or not desired.
  */
 public class ContactAddNFCActivity extends ShareWearActivity {
+    // Animation for NFC.
+    AnimationDrawable animation;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.contact_add_nfc_activity);
+
+        // Start animation.
+        ImageView animationView = (ImageView) findViewById(R.id.nfc_image);
+        animationView.setBackgroundResource(R.drawable.nfc_animation);
+        animation = (AnimationDrawable) animationView.getBackground();
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+
+        if (hasFocus) {
+            animation.start();
+        }
+        else {
+            animation.stop();
+        }
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.contact_add_menu, menu);
+        getMenuInflater().inflate(R.menu.contact_add_nfc_menu, menu);
         return true;
     }
 
