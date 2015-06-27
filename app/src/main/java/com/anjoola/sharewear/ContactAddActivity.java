4ede546755286a4db733a6a3b8c7ad6d290e83da
Angela Gong <anjoola@anjoola.com>
2015-06-27 01:52:02 -0700

    Added ability to take picture for contact photo

--- app/src/main/java/com/anjoola/sharewear/ContactAddActivity.java
+++ app/src/main/java/com/anjoola/sharewear/ContactAddActivity.java
@@ -1,12 +1,19 @@
 package com.anjoola.sharewear;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 /**
  * Activity to add new contact information manually. Can switch to input via
@@ -14,11 +21,17 @@ import android.widget.EditText;
  */
 public class ContactAddActivity extends ShareWearActivity implements
         View.OnClickListener {
+    private final int CAPTURE_IMAGE_CODE = 100;
+
     // Button for switching to NFC input.
     Button mSwitchInputButton;
 
     // EditTexts for inputs.
     EditText mName, mPhone, mEmail;
+
+    // Button for adding a photo.
+    RoundedImageView mPhoto;
+    Uri mImageFile;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -32,15 +45,28 @@ public class ContactAddActivity extends ShareWearActivity implements
         mPhone = (EditText) findViewById(R.id.person_phone);
         mEmail = (EditText) findViewById(R.id.person_email);
 
+        mPhoto = (RoundedImageView) findViewById(R.id.person_photo);
+        mPhoto.setOnClickListener(this);
+        mImageFile = null;
+
         getActionBar().setHideOnContentScrollEnabled(false);
     }
 
     @Override
     public void onClick(View v) {
+        // Switch to NFC input. Just bring the activity to the front if it has
+        // already been started.
         if (v.getId() == R.id.nfc_input_button) {
             Intent intent = new Intent(this, ContactAddNFCActivity.class);
             intent.addFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
             startActivity(intent);
+        }
+        // Start intent to take a picture.
+        else if (v.getId() == R.id.person_photo) {
+            Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            mImageFile = getImageFileUri();
+            intent.putExtra(MediaStore.EXTRA_OUTPUT, mImageFile);
+            startActivityForResult(intent, CAPTURE_IMAGE_CODE);
         }
     }
 
@@ -62,5 +88,47 @@ public class ContactAddActivity extends ShareWearActivity implements
             default:
                 return super.onOptionsItemSelected(item);
         }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        // Result from taking a picture. Set the image to the newly-taken photo.
+        if (requestCode == CAPTURE_IMAGE_CODE && resultCode == RESULT_OK &&
+                mImageFile != null) {
+            mPhoto.setImageURI(mImageFile);
+        }
+    }
+
+    /**
+     * Create a file URI for saving an image.
+     *
+     * @return Reference to the URI.
+     */
+    private Uri getImageFileUri(){
+        return Uri.fromFile(getImageFile());
+    }
+
+    /**
+     * Create a file for an image.
+     *
+     * @return Reference to the image file.
+     */
+    private File getImageFile(){
+        // Get the directory for saving images.
+        File directory = new File(Environment.getExternalStoragePublicDirectory(
+                Environment.DIRECTORY_PICTURES), getString(R.string.app_name));
+
+        // Create it if it doesn't exist.
+        if (!directory.exists()){
+            if (!directory.mkdirs()){
+                return null;
+            }
+        }
+
+        // Create file based on timestamp.
+        String timestamp = new SimpleDateFormat("yyyyMMdd-HHmmss").format(new Date());
+        File file = new File(directory.getPath() + File.separator +
+                "IMG_"+ timestamp + ".jpg");
+        return file;
     }
 }
