18dda4e46b429926ff354d5e9b38dd752be887da
Angela Gong <anjoola@anjoola.com>
2015-06-28 14:45:32 -0700

    Connected with the Contacts API

--- app/src/main/java/com/anjoola/sharewear/ContactAddActivity.java
+++ app/src/main/java/com/anjoola/sharewear/ContactAddActivity.java
@@ -1,9 +1,19 @@
 package com.anjoola.sharewear;
 
+import android.content.ContentProviderOperation;
 import android.content.Intent;
+import android.content.OperationApplicationException;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
+import android.os.RemoteException;
+import android.provider.ContactsContract;
+import android.provider.ContactsContract.CommonDataKinds.Email;
+import android.provider.ContactsContract.CommonDataKinds.Phone;
+import android.provider.ContactsContract.CommonDataKinds.Photo;
+import android.provider.ContactsContract.CommonDataKinds.StructuredName;
+import android.provider.ContactsContract.Data;
+import android.provider.ContactsContract.RawContacts;
 import android.provider.MediaStore;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -12,8 +22,11 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.FrameLayout;
 
+import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.FileInputStream;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
 
 /**
@@ -33,7 +46,8 @@ public class ContactAddActivity extends ShareWearActivity implements
     // Button for adding a photo.
     RoundedImageView mPhoto;
     FrameLayout mPhotoButton;
-    Uri mImageFile;
+    File mImageFile;
+    Uri mImageFileUri;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -51,6 +65,7 @@ public class ContactAddActivity extends ShareWearActivity implements
         mPhotoButton = (FrameLayout) findViewById(R.id.person_photo_button);
         mPhotoButton.setOnClickListener(this);
         mImageFile = null;
+        mImageFileUri = null;
 
         getActionBar().setHideOnContentScrollEnabled(false);
     }
@@ -67,8 +82,9 @@ public class ContactAddActivity extends ShareWearActivity implements
         // Start intent to take a picture.
         else if (v.getId() == R.id.person_photo_button) {
             Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-            mImageFile = getImageFileUri();
-            intent.putExtra(MediaStore.EXTRA_OUTPUT, mImageFile);
+            mImageFile = getImageFile();
+            mImageFileUri = getImageFileUri(mImageFile);
+            intent.putExtra(MediaStore.EXTRA_OUTPUT, mImageFileUri);
             startActivityForResult(intent, CAPTURE_IMAGE_CODE);
         }
     }
@@ -83,7 +99,7 @@ public class ContactAddActivity extends ShareWearActivity implements
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case R.id.action_add:
-                // TODO
+                addContact();
                 return true;
             case R.id.action_sign_out:
                 signOut();
@@ -98,17 +114,95 @@ public class ContactAddActivity extends ShareWearActivity implements
         // Result from taking a picture. Set the image to the newly-taken photo.
         if (requestCode == CAPTURE_IMAGE_CODE && resultCode == RESULT_OK &&
                 mImageFile != null) {
-            mPhoto.setImageURI(mImageFile);
+            mPhoto.setImageURI(mImageFileUri);
         }
+    }
+
+    /**
+     * Adds a new contact based on the values filled out in the text fields.
+     */
+    private void addContact() {
+        // Get filled out fields.
+        String name = mName.getText().toString();
+        String phone = mPhone.getText().toString();
+        String email = mEmail.getText().toString();
+
+        ArrayList<ContentProviderOperation> ops =
+                new ArrayList<ContentProviderOperation>();
+        int contactIdx = ops.size();
+
+        // Insert raw contact into contacts database.
+        ops.add(ContentProviderOperation.newInsert(RawContacts.CONTENT_URI)
+                .withValue(RawContacts.ACCOUNT_TYPE, null)
+                .withValue(RawContacts.ACCOUNT_NAME, null)
+                .build());
+
+        // Display name.
+        ops.add(ContentProviderOperation.newInsert(Data.CONTENT_URI)
+                .withValueBackReference(Data.RAW_CONTACT_ID, contactIdx)
+                .withValue(Data.MIMETYPE, StructuredName.CONTENT_ITEM_TYPE)
+                .withValue(StructuredName.DISPLAY_NAME, name)
+                .build());
+
+        // Phone number.
+        ops.add(ContentProviderOperation.newInsert(Data.CONTENT_URI)
+                .withValueBackReference(Data.RAW_CONTACT_ID, contactIdx)
+                .withValue(Data.MIMETYPE, Phone.CONTENT_ITEM_TYPE)
+                .withValue(Phone.NUMBER, phone)
+                .withValue(Phone.TYPE, Phone.TYPE_MOBILE)
+                .build());
+
+        // Email.
+        ops.add(ContentProviderOperation.newInsert(Data.CONTENT_URI)
+                .withValueBackReference(Data.RAW_CONTACT_ID, contactIdx)
+                .withValue(Data.MIMETYPE, Email.CONTENT_ITEM_TYPE)
+                .withValue(Email.DATA, email)
+                .withValue(Email.TYPE, Email.TYPE_OTHER)
+                .build());
+
+        // Photo.
+        if (mImageFile != null) {
+            int imgLength = (int) mImageFile.length();
+            ByteArrayOutputStream stream = new ByteArrayOutputStream(imgLength);
+            byte[] bytes = new byte[imgLength];
+            FileInputStream inStream;
+
+            // Convert photo to byte stream.
+            try {
+                inStream = new FileInputStream(mImageFile);
+                inStream.read(bytes);
+                stream.write(bytes, 0, imgLength);
+                stream.flush();
+            }
+            catch (Exception e) { }
+
+            ops.add(ContentProviderOperation.newInsert(Data.CONTENT_URI)
+                    .withValueBackReference(Data.RAW_CONTACT_ID, contactIdx)
+                    .withValue(Data.MIMETYPE, Photo.CONTENT_ITEM_TYPE)
+                    .withValue(Data.IS_SUPER_PRIMARY, 1)
+                    .withValue(Photo.PHOTO, stream.toByteArray())
+                    .build());
+        }
+
+        // Do a batch operation to insert all data.
+        try {
+            getContentResolver().applyBatch(ContactsContract.AUTHORITY, ops);
+
+            // TODO success page
+        }
+        // TODO handle exceptions
+        catch (RemoteException exp) {}
+        catch (OperationApplicationException exp) {}
     }
 
     /**
      * Create a file URI for saving an image.
      *
+     * @param file The file to create a URI from.
      * @return Reference to the URI.
      */
-    private Uri getImageFileUri(){
-        return Uri.fromFile(getImageFile());
+    private Uri getImageFileUri(File file){
+        return Uri.fromFile(file);
     }
 
     /**
