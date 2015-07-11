3c736de955d7e30450f5f232814f37cdddfd6b62
Angela Gong <anjoola@anjoola.com>
2015-07-10 18:14:54 -0700

    Added back all contacts view

--- app/src/main/java/com/anjoola/sharewear/ContactsAllFragment.java
+++ app/src/main/java/com/anjoola/sharewear/ContactsAllFragment.java
@@ -1,18 +1,247 @@
 package com.anjoola.sharewear;
 
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.MatrixCursor;
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AbsListView;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
 
-public class ContactsAllFragment extends Fragment {
+public class ContactsAllFragment extends Fragment implements
+        AdapterView.OnItemClickListener {
+    // Adapter for mapping contacts to objects in the ListViews.
+    private SimpleCursorAdapter mAdapter;
+
+    // Asynchronous task to retrieve and load ListView with contacts.
+    private ContactsListLoader mLoader;
+    private MatrixCursor mMatrixCursor;
+
+    // Number of contacts to load at once.
+    public static int LOAD_NUM = 20;
+
+    // For getting images for contacts.
+    private ContactsImageProvider mImgProvider;
+
     @Override
     public View onCreateView(LayoutInflater inflater,
                              @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.contacts_all_fragment, container, false);
+
+        mImgProvider = new ContactsImageProvider(getActivity().getBaseContext());
+
+        // Set up contacts list view. Set adapter and scroll listener for
+        // infinite scrolling.
+        ListView contactsList = (ListView) v.findViewById(R.id.contacts_list_all);
+        contactsList.setOnScrollListener(new EndlessScrollListener());
+        contactsList.setOnItemClickListener(this);
+
+        mAdapter = new SimpleCursorAdapter(getActivity(),
+                R.layout.contacts_list_view_layout,
+                null,
+                new String[] { "photo", "name" },
+                new int[] { R.id.photo, R.id.name }, 0);
+        contactsList.setAdapter(mAdapter);
+
+        // Cursor for loading all details.
+        mMatrixCursor = new MatrixCursor(
+                new String[] {"_id", "photo", "name", "phone", "email"});
+        getActivity().runOnUiThread(new Runnable() {
+            public void run() {
+                new ContactsListLoader(0).execute();
+            }
+        });
+
+        //contactsList.setFastScrollEnabled(true);
+        // contactsList.setTextFilterEnabled(true);
+
+        mAdapter.getFilter().filter("aaa");
+
+
+
         return v;
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        // Hide the search view.
+        ((ContactsListActivity) getActivity()).hideSearchView();
+
+        MatrixCursor entry = (MatrixCursor) parent.getAdapter().getItem(position);
+
+        // Send contact details over to activity.
+        Intent intent = new Intent(getActivity(), ContactViewActivity.class);
+        intent.putExtra(ShareWearActivity.PHOTO, entry.getString(1));
+        intent.putExtra(ShareWearActivity.NAME, entry.getString(2));
+        intent.putExtra(ShareWearActivity.PHONE, entry.getString(3));
+        intent.putExtra(ShareWearActivity.EMAIL, entry.getString(4));
+        startActivity(intent);
+    }
+
+    /**
+     * Asynchronous class to retrieve and load ListView with contacts.
+     */
+    private class ContactsListLoader extends AsyncTask<Void, Void, Cursor> {
+        // URIs for retrieving contacts information.
+        private final Uri CONTACTS_URI = ContactsContract.Contacts.CONTENT_URI;
+        private final Uri DATA_URI = ContactsContract.Data.CONTENT_URI;
+
+        // Cursor for retrieving all contacts.
+        private int offset;
+
+        public ContactsListLoader(int offset) {
+            this.offset = offset;
+        }
+
+        @Override
+        protected Cursor doInBackground(Void... params) {
+            Cursor cursor = getActivity().getContentResolver().query(CONTACTS_URI, null, null,
+                    null, ContactsContract.Contacts.DISPLAY_NAME + " ASC ");
+
+            // Move to the specified offset, if it exists.
+            if (!cursor.moveToPosition(offset * LOAD_NUM))
+                return null;
+
+            // Loop through every contact.
+            int numLoaded = 0;
+            do {
+                long contactId = cursor.getLong(cursor.getColumnIndex("_ID"));
+
+                // Retrieve individual fields for this contact.
+                Cursor dCursor = getActivity().getContentResolver().query(DATA_URI, null,
+                        ContactsContract.Data.CONTACT_ID + "=" + contactId,
+                        null, null);
+
+                // No data, move on.
+                if (!dCursor.moveToFirst()) continue;
+
+                getContactDetails(dCursor, contactId);
+
+            } while (++numLoaded < LOAD_NUM && cursor.moveToNext());
+
+            cursor.close();
+            return mMatrixCursor;
+        }
+
+        @Override
+        protected void onPostExecute(Cursor result) {
+            mAdapter.swapCursor(result);
+            mAdapter.notifyDataSetChanged();
+        }
+
+        /**
+         * Get details for the contact at the cursor and add it to the matrix
+         * cursor.
+         *
+         * @param cursor The cursor.
+         */
+        private void getContactDetails(Cursor cursor, long contactId) {
+            // Details to retrieve.
+            String photoUri = cursor.getString(cursor.getColumnIndex(
+                    ContactsContract.CommonDataKinds.Phone.PHOTO_URI));
+            String name = cursor.getString(cursor.getColumnIndex(
+                    ContactsContract.Contacts.DISPLAY_NAME));
+            String phone = null;
+            String email = null;
+
+            // Loop since there might be multiple entries.
+            do {
+                String columnType = cursor.getString(cursor.getColumnIndex("mimetype"));
+
+                // Phone numbers.
+                if (columnType.equals(ContactsContract.CommonDataKinds.Phone.CONTENT_ITEM_TYPE)) {
+                    switch (cursor.getInt(cursor.getColumnIndex("data2"))) {
+                        case ContactsContract.CommonDataKinds.Phone.TYPE_MOBILE:
+                            phone = cursor.getString(cursor.getColumnIndex("data1"));
+                            break;
+                        case ContactsContract.CommonDataKinds.Phone.TYPE_WORK:
+                            if (phone == null)
+                                phone = cursor.getString(cursor.getColumnIndex("data1"));
+                            break;
+                        case ContactsContract.CommonDataKinds.Phone.TYPE_HOME:
+                            if (phone == null)
+                                phone = cursor.getString(cursor.getColumnIndex("data1"));
+                            break;
+                        case ContactsContract.CommonDataKinds.Phone.TYPE_OTHER:
+                            if (phone == null)
+                                phone = cursor.getString(cursor.getColumnIndex("data1"));
+                            break;
+                    }
+                }
+
+                // Email.
+                if (columnType.equals(ContactsContract.CommonDataKinds.Email.CONTENT_ITEM_TYPE)) {
+                    switch (cursor.getInt(cursor.getColumnIndex("data2"))) {
+                        case ContactsContract.CommonDataKinds.Email.TYPE_HOME:
+                            email = cursor.getString(cursor.getColumnIndex("data1"));
+                            break;
+                        case ContactsContract.CommonDataKinds.Email.TYPE_WORK:
+                            if (email == null)
+                                email = cursor.getString(cursor.getColumnIndex("data1"));
+                            break;
+                        case ContactsContract.CommonDataKinds.Email.TYPE_OTHER:
+                            if (email == null)
+                                email = cursor.getString(cursor.getColumnIndex("data1"));
+                            break;
+                    }
+                }
+            } while (cursor.moveToNext());
+
+            // If contact has neither a phone nor email, don't show them.
+            if (phone == null && email == null) return;
+
+            // Get default photo if contact photo does not exist.
+            if (photoUri == null)
+                photoUri = mImgProvider.getDefaultContactUri(name);
+
+            // Add contact details to cursor.
+            mMatrixCursor.addRow(new Object[]{
+                    Long.toString(contactId),
+                    photoUri,
+                    name,
+                    phone,
+                    email
+            });
+        }
+    }
+
+    // TODO, doesn't work
+    private class EndlessScrollListener implements AbsListView.OnScrollListener {
+        // Offset loaded.
+        private int offset = 0;
+
+        private int previousTotal = 0;
+        private boolean loading = false;
+
+
+        @Override
+        public void onScroll(AbsListView view, int firstVisibleItem,
+                             int visibleItemCount, int totalItemCount) {
+            if (loading && totalItemCount > previousTotal) {
+                loading = false;
+            }
+            if (!loading && (firstVisibleItem + visibleItemCount - 2 >= previousTotal + LOAD_NUM - 10)) {
+                getActivity().runOnUiThread(new Runnable() {
+                    public void run() {
+                        new ContactsListLoader(++offset).execute();
+                        previousTotal += LOAD_NUM;
+                        loading = true;
+                    }
+                });
+            }
+        }
+
+        @Override
+        public void onScrollStateChanged(AbsListView view, int scrollState) { }
     }
 }
--- app/src/main/java/com/anjoola/sharewear/ContactsListActivity.java
+++ app/src/main/java/com/anjoola/sharewear/ContactsListActivity.java
@@ -3,55 +3,27 @@ package com.anjoola.sharewear;
 import android.app.SearchManager;
 import android.content.Context;
 import android.content.Intent;
-import android.database.Cursor;
-import android.database.MatrixCursor;
-import android.net.Uri;
-import android.os.AsyncTask;
 import android.os.Bundle;
-import android.provider.ContactsContract;
-import android.provider.ContactsContract.CommonDataKinds.Phone;
 import android.support.v4.view.ViewPager;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.AbsListView;
-import android.widget.AdapterView;
-import android.widget.AdapterView.OnItemClickListener;
 import android.widget.SearchView;
-import android.widget.SimpleCursorAdapter;
 
 /**
  * Displays list of contacts and favorites. Allows user to add and search
  * for contacts. Floating action button allows user to share their location.
+ *
+ * Tab layout courtesy of http://www.tutorialsbuzz.com/2015/04/Android-Material-Design-Sliding-TabLayout.html.
  */
 public class ContactsListActivity extends ShareWearActivity implements
-        SearchView.OnQueryTextListener, View.OnClickListener, OnItemClickListener {
+        SearchView.OnQueryTextListener, View.OnClickListener {
     // Floating action button for getting current location.
     android.support.design.widget.FloatingActionButton mFab;
-
-    // Adapter for mapping contacts to objects in the ListViews.
-    private SimpleCursorAdapter mFavoritesAdapter, mAdapter;
-    private DoubleListAdapter mDoubleAdapter;
-
-    // Asynchronous task to retrieve and load ListView with contacts.
-    private ContactsListLoader mLoader;
-    private MatrixCursor mMatrixCursor;
-
-    // Number of contacts to load at once.
-    public static int LOAD_NUM = 20;
-
-    // For getting images for contacts.
-    private ContactsImageProvider mImgProvider;
 
     // For searching.
     MenuItem mSearchMenuItem;
     SearchView mSearchView;
-
-
-    // TODO
-    ViewPager pager;
-    ViewPagerAdapter adapter;
-    SlidingTabLayout tabs;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -61,80 +33,26 @@ public class ContactsListActivity extends ShareWearActivity implements
         // Set up handler for getting current location floating action button.
         mFab = (android.support.design.widget.FloatingActionButton)
                 findViewById(R.id.fab_my_location);
-        //mFab.setOnClickListener(this);
+        mFab.setOnClickListener(this);
 
-        mImgProvider = new ContactsImageProvider(getBaseContext());
+        // Hide action bar shadow.
+        if (getActionBar() != null)
+            getActionBar().setElevation(0);
 
-        // Set up contacts list view. Set adapter and scroll listener for
-        // infinite scrolling.
-       // ListView contactsList = (ListView) findViewById(R.id.contacts_list);
-       // contactsList.setOnScrollListener(new EndlessScrollListener());
-       // contactsList.setOnItemClickListener(this);
-
-        mFavoritesAdapter = new SimpleCursorAdapter(this,
-                R.layout.contacts_list_view_favorite,
-                null,
-                new String[] { "photo", "name" },
-                new int[] { R.id.photo, R.id.name }, 0);
-        mAdapter = new SimpleCursorAdapter(this,
-                R.layout.contacts_list_view_layout,
-                null,
-                new String[] { "photo", "name" },
-                new int[] { R.id.photo, R.id.name }, 0);
-        mDoubleAdapter = new DoubleListAdapter(this,
-                getString(R.string.favorites), mFavoritesAdapter,
-                getString(R.string.all_contacts), mAdapter);
-       // contactsList.setAdapter(mDoubleAdapter);
-
-        // Cursor for loading all details.
-        mMatrixCursor = new MatrixCursor(
-                new String[] {"_id", "photo", "name", "phone", "email"});
-        runOnUiThread(new Runnable() {
-            public void run() {
-                new ContactsListLoader(0).execute();
-            }
-        });
-
-        //contactsList.setFastScrollEnabled(true);
-       // contactsList.setTextFilterEnabled(true);
-
-        mAdapter.getFilter().filter("aaa");
-
-        getActionBar().setElevation(0);
-
-        // Creating The ViewPagerAdapter and Passing Fragment Manager, Titles
-        // fot the Tabs and Number Of Tabs.
-        CharSequence Titles[] = {
-                getString(R.string.favorites),
-                getString(R.string.all_contacts)
+        // Create tabs.
+        CharSequence tabTitles[] = {
+            getString(R.string.favorites),
+            getString(R.string.all_contacts)
         };
-        adapter = new ViewPagerAdapter(getSupportFragmentManager(), Titles);
-
-
-
-
-        // Assigning ViewPager View and setting the adapter
-        pager = (ViewPager) findViewById(R.id.pager);
-        pager.setAdapter(adapter);
-
-        // Assiging the Sliding Tab Layout View
-        tabs = (SlidingTabLayout) findViewById(R.id.tabs);
+        SlidingTabLayout tabs = (SlidingTabLayout) findViewById(R.id.tabs);
         tabs.setDistributeEvenly(true);
 
-
-
-        // Setting Custom Color for the Scroll bar indicator of the Tab View
-//        tabs.setCustomTabColorizer(new SlidingTabLayout.TabColorizer() {
-//            @Override
-//            public int getIndicatorColor(int position) {
-//
-//                return R.color.Black;
-//            }
-//        });
-
-        // Setting the ViewPager For the SlidingTabsLayout
+        // Page adapter for sliding view.
+        ViewPagerAdapter adapter =
+                new ViewPagerAdapter(getSupportFragmentManager(), tabTitles);
+        ViewPager pager = (ViewPager) findViewById(R.id.pager);
+        pager.setAdapter(adapter);
         tabs.setViewPager(pager);
-
     }
 
     @Override
@@ -146,25 +64,6 @@ public class ContactsListActivity extends ShareWearActivity implements
     }
 
     @Override
-    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-        // Hide search view if it is shown.
-        if (mSearchView.isShown()) {
-            mSearchMenuItem.collapseActionView();
-            mSearchView.setQuery("", false);
-        }
-
-        MatrixCursor entry = (MatrixCursor) parent.getAdapter().getItem(position);
-
-        // Send contact details over to activity.
-        Intent intent = new Intent(this, ContactViewActivity.class);
-        intent.putExtra(ShareWearActivity.PHOTO, entry.getString(1));
-        intent.putExtra(ShareWearActivity.NAME, entry.getString(2));
-        intent.putExtra(ShareWearActivity.PHONE, entry.getString(3));
-        intent.putExtra(ShareWearActivity.EMAIL, entry.getString(4));
-        startActivity(intent);
-    }
-
-    @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.contacts_list_menu, menu);
 
@@ -173,7 +72,6 @@ public class ContactsListActivity extends ShareWearActivity implements
         mSearchView = (SearchView) mSearchMenuItem.getActionView();
 
         mSearchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
-        mSearchView.setSubmitButtonEnabled(true);
         mSearchView.setOnQueryTextListener(this);
         return true;
     }
@@ -204,161 +102,12 @@ public class ContactsListActivity extends ShareWearActivity implements
     }
 
     /**
-     * Asynchronous class to retrieve and load ListView with contacts.
+     * Hides the search view if it is shown.
      */
-    private class ContactsListLoader extends AsyncTask<Void, Void, Cursor> {
-        // URIs for retrieving contacts information.
-        private final Uri CONTACTS_URI = ContactsContract.Contacts.CONTENT_URI;
-        private final Uri DATA_URI = ContactsContract.Data.CONTENT_URI;
-
-        // Cursor for retrieving all contacts.
-        private int offset;
-
-        public ContactsListLoader(int offset) {
-            this.offset = offset;
+    public void hideSearchView() {
+        if (mSearchView.isShown()) {
+            mSearchMenuItem.collapseActionView();
+            mSearchView.setQuery("", false);
         }
-
-        @Override
-        protected Cursor doInBackground(Void... params) {
-            Cursor cursor = getContentResolver().query(CONTACTS_URI, null, null,
-                    null, ContactsContract.Contacts.DISPLAY_NAME + " ASC ");
-
-            // Move to the specified offset, if it exists.
-            if (!cursor.moveToPosition(offset * LOAD_NUM))
-                return null;
-
-            // Loop through every contact.
-            int numLoaded = 0;
-            do {
-                long contactId = cursor.getLong(cursor.getColumnIndex("_ID"));
-
-                // Retrieve individual fields for this contact.
-                Cursor dCursor = getContentResolver().query(DATA_URI, null,
-                        ContactsContract.Data.CONTACT_ID + "=" + contactId,
-                        null, null);
-
-                // No data, move on.
-                if (!dCursor.moveToFirst()) continue;
-
-                getContactDetails(dCursor, contactId);
-
-            } while (++numLoaded < LOAD_NUM && cursor.moveToNext());
-
-            cursor.close();
-            return mMatrixCursor;
-        }
-
-        @Override
-        protected void onPostExecute(Cursor result) {
-            // TODO get favorites
-            //mFavoritesAdapter.swapCursor(result);
-            mAdapter.swapCursor(result);
-            mDoubleAdapter.notifyDataSetChanged();
-        }
-
-        /**
-         * Get details for the contact at the cursor and add it to the matrix
-         * cursor.
-         *
-         * @param cursor The cursor.
-         */
-        private void getContactDetails(Cursor cursor, long contactId) {
-            // Details to retrieve.
-            String photoUri = cursor.getString(cursor.getColumnIndex(
-                    Phone.PHOTO_URI));
-            String name = cursor.getString(cursor.getColumnIndex(
-                    ContactsContract.Contacts.DISPLAY_NAME));
-            String phone = null;
-            String email = null;
-
-            // Loop since there might be multiple entries.
-            do {
-                String columnType = cursor.getString(cursor.getColumnIndex("mimetype"));
-
-                // Phone numbers.
-                if (columnType.equals(Phone.CONTENT_ITEM_TYPE)) {
-                    switch (cursor.getInt(cursor.getColumnIndex("data2"))) {
-                        case Phone.TYPE_MOBILE:
-                            phone = cursor.getString(cursor.getColumnIndex("data1"));
-                            break;
-                        case Phone.TYPE_WORK:
-                            if (phone == null)
-                                phone = cursor.getString(cursor.getColumnIndex("data1"));
-                            break;
-                        case Phone.TYPE_HOME:
-                            if (phone == null)
-                                phone = cursor.getString(cursor.getColumnIndex("data1"));
-                            break;
-                        case Phone.TYPE_OTHER:
-                            if (phone == null)
-                                phone = cursor.getString(cursor.getColumnIndex("data1"));
-                            break;
-                    }
-                }
-
-                // Email.
-                if (columnType.equals(ContactsContract.CommonDataKinds.Email.CONTENT_ITEM_TYPE)) {
-                    switch (cursor.getInt(cursor.getColumnIndex("data2"))) {
-                        case ContactsContract.CommonDataKinds.Email.TYPE_HOME:
-                            email = cursor.getString(cursor.getColumnIndex("data1"));
-                            break;
-                        case ContactsContract.CommonDataKinds.Email.TYPE_WORK:
-                            if (email == null)
-                                email = cursor.getString(cursor.getColumnIndex("data1"));
-                            break;
-                        case ContactsContract.CommonDataKinds.Email.TYPE_OTHER:
-                            if (email == null)
-                                email = cursor.getString(cursor.getColumnIndex("data1"));
-                            break;
-                    }
-                }
-            } while (cursor.moveToNext());
-
-            // If contact has neither a phone nor email, don't show them.
-            if (phone == null && email == null) return;
-
-            // Get default photo if contact photo does not exist.
-            if (photoUri == null)
-                photoUri = mImgProvider.getDefaultContactUri(name);
-
-            // Add contact details to cursor.
-            mMatrixCursor.addRow(new Object[]{
-                    Long.toString(contactId),
-                    photoUri,
-                    name,
-                    phone,
-                    email
-            });
-        }
-    }
-
-    // TODO, doesn't work
-    private class EndlessScrollListener implements AbsListView.OnScrollListener {
-        // Offset loaded.
-        private int offset = 0;
-
-        private int previousTotal = 0;
-        private boolean loading = false;
-
-
-        @Override
-        public void onScroll(AbsListView view, int firstVisibleItem,
-                             int visibleItemCount, int totalItemCount) {
-            if (loading && totalItemCount > previousTotal) {
-                loading = false;
-            }
-            if (!loading && (firstVisibleItem + visibleItemCount - 2 >= previousTotal + LOAD_NUM - 10)) {
-                runOnUiThread(new Runnable() {
-                    public void run() {
-                        new ContactsListLoader(++offset).execute();
-                        previousTotal += LOAD_NUM;
-                        loading = true;
-                    }
-                });
-            }
-        }
-
-        @Override
-        public void onScrollStateChanged(AbsListView view, int scrollState) { }
     }
 }
