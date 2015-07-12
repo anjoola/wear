95276a42cfdf26c0780d1495e93dce133657a906
Angela Gong <anjoola@anjoola.com>
2015-07-12 02:31:41 -0700

    Updated server to have debug stuff

--- server/server.py
+++ server/server.py
@@ -11,6 +11,8 @@ Naive server. Keeps stuff in memory. TODO
 '''
 class ShareWearServerHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
 
+    users = []
+
     def do_POST(self):
         header = self.headers
 
@@ -29,30 +31,31 @@ class ShareWearServerHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
             # New user.
             if cmd == "new_user":
                 # TODO
-                pass
+                self.users.append(json_data["name"])
 
             # Location request from one user to another.
             elif cmd == "location_request":
-                user_from = json_data["user_id"]
-                user_to = json_data["user_to"]
+                user_from = json_data["id"]
+                user_to = json_data["to"]
                 # TODO
 
             # User updating with their current location.
             elif cmd == "location_add":
-                user = json_data["user_id"]
+                user = json_data["id"]
                 lat = float(json_data["lat"])
 		lng = float(json_data["lng"])
                 # TODO
 
             # User clearing all location data.
             elif cmd == "location_clear":
-                user = json_data["user_id"]
+                user = json_data["id"]
                 # TODO
 
             self.send_response(200)
-            self.end_headers()
+            print "users now: " + str(self.users)
 
-        except:	
+        except Exception as e:
+            print e
             self.send_response(422)
             return
 
